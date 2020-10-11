3954afa2350b9292e46efedad8bd0f5a72c69bb3
Sagar Shrestha <sg28r8@gmail.com>
2020-10-10 22:17:54 -0400

    fuctioning server with a single GET request ep

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ go 1.15
 
 require (
 	be_good/src/db v0.0.0
+	github.com/go-redis/redis/v8 v8.2.3
 	github.com/gorilla/mux v1.8.0
-	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.8.0 // indirect
 )
 
--- src/db/db.go
+++ src/db/db.go
@@ -10,15 +10,20 @@ import (
 )
 
 type Job struct {
-	Id              string `json:"id"`
-	JobTitle        string `json:"job_title"`
-	ApplyLink       string `json:"apply_link"`
-	CompanyName     string `json:"cmp_name"`
-	CompanyLocation string `json:"cmp_location"`
-	JobDescription  string `json:"job_description"`
+	Id              string  `json:"Id"`
+	Companyname     string  `json:"Cmp_name"`
+	Companylocation string  `json:"Cmp_location"`
+	Jobtitle        string  `json:"Job_title"`
+	Jobdescription  string  `json:"Job_description"`
+	Applylink       string  `json:"Apply_link"`
+	Timestamps      float32 `json: "Timestamps"`
 }
 
-func GetData() Job {
+//GetData returns array of Job structs
+func GetData(limit int8) []Job {
+	//returns data from postgres db
+	returnArray := make([]Job, limit)
+	count := 0
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -26,7 +31,7 @@ func GetData() Job {
 	password := os.Getenv("USER_PASS")
 	user := os.Getenv("USER_NAME")
 	dbname := "career_bot"
-	psqlInfo := fmt.Sprintf("host=13.92.43.146 port=5432 user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
+	psqlInfo := fmt.Sprintf("host=52.188.71.209 port=5432 user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
 	fmt.Println(psqlInfo)
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
@@ -38,11 +43,21 @@ func GetData() Job {
 		panic(err)
 	}
 	fmt.Println("Sucessfully connected")
-	var myJob Job
-	userSql := "SELECT * FROM cs_bot.Jobs"
-	err = db.QueryRow(userSql).Scan(&myJob.ApplyLink, &myJob.CompanyName, &myJob.CompanyLocation, &myJob.JobDescription, &myJob.JobTitle, &myJob.Id)
+	userSql := fmt.Sprintf("SELECT * FROM cs_bot.Jobs limit %d", limit)
+	rows, err := db.Query(userSql)
+	defer rows.Close()
+	for rows.Next() {
+		var myJob Job
+		err := rows.Scan( &myJob.Id,&myJob.Companyname, &myJob.Companylocation, &myJob.Jobtitle, &myJob.Jobdescription, &myJob.Applylink, &myJob.Timestamps)
+		if err != nil {
+			log.Fatal("Couldn't execute for some odd reason", err)
+		}
+		returnArray[count] = myJob
+		count++
+
+	}
 	if err != nil {
 		log.Fatal("Couldn't execute for some odd reason", err)
 	}
-	return myJob
+	return returnArray
 }
